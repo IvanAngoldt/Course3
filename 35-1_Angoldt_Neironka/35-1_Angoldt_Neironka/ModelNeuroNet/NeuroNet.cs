a43ab20f15e0c2ba7d0eb746f61492157b8f973f
Ivan <112856964+IvanAngoldt@users.noreply.github.com>
2023-10-10 15:29:19 +0300

    Add files via upload

---
+++ 35-1_Angoldt_Neironka/35-1_Angoldt_Neironka/ModelNeuroNet/NeuroNet.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _35_1_Angoldt_Neironka.ModelNeuroNet
+{
+    class NeuroNet
+    {
+
+        public NeuroNet()
+        { 
+        
+        }
+    }
+}
---
+++ 35-1_Angoldt_Neironka/35-1_Angoldt_Neironka/ModelNeuroNet/Neuron.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _35_1_Angoldt_Neironka.ModelNeuroNet
+{
+    class Neuron
+    {
+        // поля
+        TypeNeuron type;
+        double[] weight;
+        double[] inputdata;
+        double outputdata;
+
+        // свойства
+        public double[] Weight
+        {
+            get { return weight; }
+            set { weight = value; }
+        }      
+        
+        public double[] InputData
+        {
+            get { return inputdata; }
+            set { inputdata = value; }
+        }
+
+        // конструктор
+        public Neuron(TypeNeuron typeNeuron, double[] wght, double[] inpt)
+        { 
+            type = typeNeuron;
+            weight = wght;
+            inputdata = inpt;
+        }
+
+        public void Activator() 
+        {
+        
+        }
+    }
+}
