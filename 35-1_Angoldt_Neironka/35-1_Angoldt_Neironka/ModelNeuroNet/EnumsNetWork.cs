1313b8cf673cc72887d67c5bd0cc55aa0f49b120
Ivan <112856964+IvanAngoldt@users.noreply.github.com>
2023-10-24 15:21:25 +0300

    Add files via upload

---
+++ 35-1_Angoldt_Neironka/35-1_Angoldt_Neironka/ModelNeuroNet/EnumsNetWork.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _35_1_Angoldt_Neironka.ModelNeuroNet
+{
+    enum TypeNeuron { Hidden, Output }
+    enum MemoryMod { Get, Set, Init }
+}
